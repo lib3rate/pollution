771298844ed22a4775d6d5f2c76945f67fd12525
Dimitri Petrov <e.liberate@gmail.com>
2024-04-18 23:32:53 -0400

    Update boilerplate app

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,28 @@
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
-}
-
-struct Model {
-    // `ComponentLink` is like a reference to a component.
-    // It can be used to send messages to the component
-    link: ComponentLink<Self>,
-    value: i64,
-}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self {
-            link,
-            value: 0,
-        }
+#[function_component]
+fn App() -> Html {
+  let counter = use_state(|| 0);
+  let onclick = {
+    let counter = counter.clone();
+    move |_| {
+      let value = *counter + 1;
+      counter.set(value);
     }
+  };
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
-    }
+  html! {
+    <div>
+      <button {onclick}>
+        { "+1" }
+      </button>
+      <p>
+        { *counter }
+      </p>
+    </div>
+  }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+  yew::Renderer::<App>::new().render();
 }
